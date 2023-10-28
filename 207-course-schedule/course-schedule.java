36c2b6b487d5258b48ff9cdb5d1aaa77509698a8
Kevin <kevinabishekjad@gmail.com>
2023-10-29 00:24:00 +0530

    Time: 3 ms (94.93%) | Memory: 44.3 MB (29.74%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,28 +1,33 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] indegree = new int[numCourses];
-        for(int[] a : prerequisites)
-            indegree[a[0]]++;
-        // course without deps
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i] == 0){
-                q.offer(i);
-            }
+   public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer>[] graph = new List[numCourses];
+        for(int i = 0;i<numCourses;i++){
+            graph[i] = new ArrayList<>();
         }
-        int coursewithoutdeps = q.size();
-        while(!q.isEmpty()){
-            int c = q.remove();
-            for(int[] a : prerequisites){
-                if(a[1] == c){
-                    indegree[a[0]]--;
-                    if(indegree[a[0]] == 0){
-                        coursewithoutdeps++;
-                        q.offer(a[0]);
-                    }
-                }
-            }
+        for(int[] a: prerequisites){
+            graph[a[0]].add(a[1]);
         }
-        return coursewithoutdeps == numCourses;
+        
+        boolean[] visited = new boolean[numCourses];
+        boolean[] onCurrent = new boolean[numCourses];
+        
+        for(int i = 0; i<numCourses; i++){
+            if(checkCycle(i, graph, visited, onCurrent)) return false;
+        }
+        return true;
+    }
+
+    private boolean checkCycle(int i, List<Integer>[] graph, boolean[] visited, boolean[] onCurrent) {
+        if(visited[i]) return false;
+        if(onCurrent[i]) return true;
+        
+        onCurrent[i] = true;
+        
+        for (int v : graph[i]){
+            if(checkCycle(v, graph, visited, onCurrent)) return true;
+        }
+        onCurrent[i] = false;
+        visited[i] = true;
+        return false;
     }
 }
