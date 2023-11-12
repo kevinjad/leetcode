b2d82958dd87c44d0d1e7a2a686213307e12ab57
Kevin <kevinabishekjad@gmail.com>
2023-11-12 22:04:02 +0530

    Time: 0 ms (100.00%) | Memory: 39.7 MB (65.96%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,15 +1,27 @@
 class Solution {
-    public int rob(int[] nums) {
-        int[] mem = new int[nums.length];
-        Arrays.fill(mem,-1);
-        return robHelper(nums, mem,0);
+    public int rob(int[] nums){
+        int n = nums.length;
+        int one = 0;
+        int two = 0;
+        for(int i = 0;i<n;i++){
+            int result = Math.max(two+nums[i], one);
+            two = one;
+            one = result;
+            
+        }
+        return one;
     }
+    // public int rob(int[] nums) {
+    //     int[] mem = new int[nums.length];
+    //     Arrays.fill(mem,-1);
+    //     return robHelper(nums, mem,0);
+    // }
     
-    public int robHelper(int[] nums, int[] mem, int i){
-        if(i >= nums.length) return 0;
-        if(mem[i] != -1) return mem[i];
-        int result = Math.max(robHelper(nums,mem,i+2)+nums[i], robHelper(nums,mem,i+1));
-        mem[i] = result;
-        return result;
-    }
+    // public int robHelper(int[] nums, int[] mem, int i){
+    //     if(i >= nums.length) return 0;
+    //     if(mem[i] != -1) return mem[i];
+    //     int result = Math.max(robHelper(nums,mem,i+2)+nums[i], robHelper(nums,mem,i+1));
+    //     mem[i] = result;
+    //     return result;
+    // }
 }
