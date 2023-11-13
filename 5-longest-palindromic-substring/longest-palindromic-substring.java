dad17398fcf755c9b670a121d7ccf2b5980902f2
Kevin <kevinabishekjad@gmail.com>
2023-11-13 21:12:41 +0530

    Time: 155 ms (27.19%) | Memory: 55.3 MB (5.13%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,43 +1,73 @@
 class Solution {
-   public String longestPalindrome(String s) {
+    public String longestPalindrome(String s) {
         int start = 0;
         int end = 0;
         int currentMax = 0;
         int[][] mem = new int[s.length()][s.length()];
         for(int i = 0; i<s.length();i++){
-            for(int j = i; j<s.length();j++){
-                if(isPalindrome(i,j,s, mem)){
-                    if(currentMax < (j-i+1)){
-                        currentMax = j-i+1;
+            mem[i][i] = 1;
+        }
+        for(int i = 0;i<s.length()-1;i++){
+            if(s.charAt(i) == s.charAt(i+1)){
+                mem[i][i+1] = 1;
+                start = i;
+                end = i+1;
+            }
+        }
+        for(int j = 2;j<s.length();j++){
+            for(int i = 0;i+j<s.length();i++){
+                if(s.charAt(i) == s.charAt(i+j)){
+                    if(mem[i+1][i+j-1] == 1){
+                        mem[i][i+j] = 1;
                         start = i;
-                        end = j;
+                        end = i+j;
                     }
                 }
             }
         }
+
+
         return s.substring(start,end+1);
     }
+//    public String longestPalindrome(String s) {
+//         int start = 0;
+//         int end = 0;
+//         int currentMax = 0;
+//         int[][] mem = new int[s.length()][s.length()];
+//         for(int i = 0; i<s.length();i++){
+//             for(int j = i; j<s.length();j++){
+//                 if(isPalindrome(i,j,s, mem)){
+//                     if(currentMax < (j-i+1)){
+//                         currentMax = j-i+1;
+//                         start = i;
+//                         end = j;
+//                     }
+//                 }
+//             }
+//         }
+//         return s.substring(start,end+1);
+//     }
 
-    private boolean isPalindrome(int i, int j, String s, int[][] mem) {
-        if(i>=s.length() || j <0 || j<i) return false;
-        if(i == j) return true;
-        if(i+1 == j) return s.charAt(i) == s.charAt(j);
+//     private boolean isPalindrome(int i, int j, String s, int[][] mem) {
+//         if(i>=s.length() || j <0 || j<i) return false;
+//         if(i == j) return true;
+//         if(i+1 == j) return s.charAt(i) == s.charAt(j);
         
-        if(mem[i][j] != 0){
-            return mem[i][j] ==1;
-        }
+//         if(mem[i][j] != 0){
+//             return mem[i][j] ==1;
+//         }
 
-        if(s.charAt(i) == s.charAt(j)){
-            if(isPalindrome(i+1,j-1,s,mem)){
-                mem[i][j] = 1;
-                return true;
-            } else {
-                mem[i][j] = 2;
-                return false;
-            }
-        } else {
-            mem[i][j] = 2;
-            return false;
-        }
-    }
+//         if(s.charAt(i) == s.charAt(j)){
+//             if(isPalindrome(i+1,j-1,s,mem)){
+//                 mem[i][j] = 1;
+//                 return true;
+//             } else {
+//                 mem[i][j] = 2;
+//                 return false;
+//             }
+//         } else {
+//             mem[i][j] = 2;
+//             return false;
+//         }
+//     }
 }
