38606c562bf606671e42334643ce5922e13e3eaa
Kevin <kevinabishekjad@gmail.com>
2023-12-19 23:49:37 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (50.33%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,5 +1,7 @@
 public class Solution {
     // you need treat n as an unsigned value
+
+    //Yes, it will add an extra left shift. You must keep in mind that the left shift on result is making room for the one's place computation. If you left shift in the last iteration, it preps result for the next computation, although there would be none. So, it is better to prep the result before the check so that there is no extra left shift.
     public int reverseBits(int n) {
         int result = 0;
         for(int i = 0;i<32;i++){
