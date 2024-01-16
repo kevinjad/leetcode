a2254d098e81af89dc96416cadf123ef20642519
Kevin <kevinabishekjad@gmail.com>
2024-01-16 23:47:21 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (8.92%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int cmax = 0, cmin = 0;
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                cmax++;
+                cmin++;
+            } else if (c == ')'){
+                cmax--;
+                cmin--;
+            } else {
+                cmax++;
+                cmin--;
+            }
+            if(cmax < 0) return false;
+            cmin = Math.max(cmin,0);
+        }
+        return cmin == 0;
+    }
+}
