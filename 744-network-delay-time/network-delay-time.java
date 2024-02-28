d83f41c586bb7b568631d7000b4b66595d6cfe7b
Kevin <kevinabishekjad@gmail.com>
2024-02-29 00:07:30 +0530

    Time: 14 ms (70.71%) | Memory: 48.8 MB (31.65%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,28 +1,49 @@
 class Solution {
+    // public int networkDelayTime(int[][] times, int N, int k) {
+    //     int[] dist = new int[N+1];
+    //     Arrays.fill(dist,Integer.MAX_VALUE);
+    //     dist[k] = 0;
+    //     dist[0] = 0;
+    //     boolean[] visited = new boolean[N+1];
+    //     Map<Integer, List<int[]>> adj = new HashMap<>();
+    //     for(int[] t : times){
+    //         adj.putIfAbsent(t[0], new ArrayList<>());
+    //         adj.get(t[0]).add(new int[]{t[1],t[2]});
+    //     }
+    //     PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1]-b[1]);
+    //     pq.add(new int[]{k,0});
+    //     while(!pq.isEmpty()){
+    //         int[] e = pq.poll();
+    //         int x = e[0];
+    //         if (visited[x]) continue;
+    //         visited[x] = true;
+    //         dist[x] = Math.min(dist[x], e[1]);
+    //         if(adj.get(x) == null) continue;
+    //         for(int[] yy : adj.get(x)){
+    //             int d = yy[1];
+    //             if(dist[yy[0]] > d+e[1] && !visited[yy[0]])
+    //             pq.add(new int[]{yy[0],d+e[1]});
+    //         }
+    //     }
+    //     int minVal = Integer.MIN_VALUE;
+    //     for(int p : dist){
+    //         minVal = Math.max(minVal, p);
+    //     }
+    //     return minVal == Integer.MAX_VALUE ? -1 : minVal;
+    // }
+
+
     public int networkDelayTime(int[][] times, int N, int k) {
         int[] dist = new int[N+1];
         Arrays.fill(dist,Integer.MAX_VALUE);
         dist[k] = 0;
         dist[0] = 0;
-        boolean[] visited = new boolean[N+1];
-        Map<Integer, List<int[]>> adj = new HashMap<>();
-        for(int[] t : times){
-            adj.putIfAbsent(t[0], new ArrayList<>());
-            adj.get(t[0]).add(new int[]{t[1],t[2]});
-        }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1]-b[1]);
-        pq.add(new int[]{k,0});
-        while(!pq.isEmpty()){
-            int[] e = pq.poll();
-            int x = e[0];
-            if (visited[x]) continue;
-            visited[x] = true;
-            dist[x] = Math.min(dist[x], e[1]);
-            if(adj.get(x) == null) continue;
-            for(int[] yy : adj.get(x)){
-                int d = yy[1];
-                if(dist[yy[0]] > d+e[1] && !visited[yy[0]])
-                pq.add(new int[]{yy[0],d+e[1]});
+        //non intuitive
+        for(int i = 0; i<N;i++){
+            for(int[] e : times){
+                int m = e[0]; int n = e[1]; int signal = e[2];
+                if(dist[m] == Integer.MAX_VALUE) continue;
+                dist[n] = Math.min(dist[n],dist[m]+signal);
             }
         }
         int minVal = Integer.MIN_VALUE;
