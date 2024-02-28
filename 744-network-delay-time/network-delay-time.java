0442db9ec924c67f920eaec67edfb8a250db2698
Kevin <kevinabishekjad@gmail.com>
2024-02-29 00:04:06 +0530

    Time: 52 ms (5.98%) | Memory: 47.3 MB (82.85%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -4,6 +4,7 @@ class Solution {
         Arrays.fill(dist,Integer.MAX_VALUE);
         dist[k] = 0;
         dist[0] = 0;
+        boolean[] visited = new boolean[N+1];
         Map<Integer, List<int[]>> adj = new HashMap<>();
         for(int[] t : times){
             adj.putIfAbsent(t[0], new ArrayList<>());
@@ -14,11 +15,12 @@ class Solution {
         while(!pq.isEmpty()){
             int[] e = pq.poll();
             int x = e[0];
+            visited[x] = true;
             dist[x] = Math.min(dist[x], e[1]);
             if(adj.get(x) == null) continue;
             for(int[] yy : adj.get(x)){
                 int d = yy[1];
-                if(dist[yy[0]] > d+e[1])
+                if(dist[yy[0]] > d+e[1] && !visited[yy[0]])
                 pq.add(new int[]{yy[0],d+e[1]});
             }
         }
