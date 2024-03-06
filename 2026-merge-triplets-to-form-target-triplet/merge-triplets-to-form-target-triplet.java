ff5b2acab22ef1c728172e971c2807453347db41
Kevin <kevinabishekjad@gmail.com>
2024-03-06 21:47:03 +0530

    Time: 5 ms (68.79%) | Memory: 122.5 MB (24.13%) - LeetSync

---
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+        boolean firstMatch = false;
+        boolean secondMatch = false;
+        boolean thirdMatch = false;
+        for(int[] t : triplets){
+            if(t[0]<=target[0] && t[1]<=target[1] && t[2]<=target[2]){
+                if(t[0] == target[0]) firstMatch = true;
+                if(t[1] == target[1]) secondMatch = true;
+                if(t[2] == target[2]) thirdMatch = true;
+            }
+        } 
+        return firstMatch && secondMatch && thirdMatch;
+    }
+}
