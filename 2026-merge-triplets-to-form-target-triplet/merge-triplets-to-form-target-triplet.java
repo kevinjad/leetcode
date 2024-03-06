6b873e1365c07ccac45b031f00b34108198a3df3
Kevin <kevinabishekjad@gmail.com>
2024-03-06 21:56:25 +0530

    Time: 1 ms (98.44%) | Memory: 122.4 MB (28.81%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.java
@@ -8,6 +8,8 @@ class Solution {
                 if(t[0] == target[0]) firstMatch = true;
                 if(t[1] == target[1]) secondMatch = true;
                 if(t[2] == target[2]) thirdMatch = true;
+
+                if(firstMatch && secondMatch && thirdMatch) return true;
             }
         } 
         return firstMatch && secondMatch && thirdMatch;
