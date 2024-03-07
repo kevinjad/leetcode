fc74224e9bd47dfd21a0783859831e3dd4acebdc
Kevin <kevinabishekjad@gmail.com>
2024-03-07 21:39:12 +0530

    Time: 4 ms (74.57%) | Memory: 44.7 MB (28.97%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,14 +1,25 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] footPrint = new int[26];
-        for(char c : s.toCharArray()){
-            footPrint[c-'a']++;
-        }
-        for(char c : t.toCharArray()){
-            footPrint[c-'a']--;
-        }
-        for(int i = 0;i<26;i++){
-            if(footPrint[i] != 0) return false;
+        // int[] footPrint = new int[26]; //[0 0 0 0 .. 0 0]
+        // for(char c : s.toCharArray()){
+        //     footPrint[c-'a']++; // [ 1 0 1 2 0 0..]
+        // }
+
+        // for(char c : t.toCharArray()){
+        //     footPrint[c-'a']--;
+        // }
+        // for(int i = 0;i<26;i++){
+        //     if(footPrint[i] != 0) return false;
+        // }
+        // return true;
+
+        if(s.length() != t.length()) return false;
+        char[] ss = s.toCharArray();
+        Arrays.sort(ss);
+        char[] tt = t.toCharArray();
+        Arrays.sort(tt);
+        for(int i = 0;i<ss.length;i++){
+            if(tt[i] != ss[i]) return false;
         }
         return true;
     }
