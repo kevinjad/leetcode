1404e4f5dba6cf7890447b49f364ffd58f4f78c9
Kevin <kevinabishekjad@gmail.com>
2024-03-08 22:23:42 +0530

    Time: 6 ms (96.35%) | Memory: 47.7 MB (55.79%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,14 +1,23 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> ans = new HashMap<>();
+        // for(String s : strs){
+        //     char[] footPrint = new char[26];
+        //     for(char c : s.toCharArray()){
+        //         footPrint[c-'a']++;
+        //     }
+        //     String footPrintString = String.valueOf(footPrint);
+        //     ans.putIfAbsent(footPrintString, new ArrayList<>());
+        //     ans.get(footPrintString).add(s);
+        // }
+        // return new ArrayList<>(ans.values());
+
         for(String s : strs){
-            char[] footPrint = new char[26];
-            for(char c : s.toCharArray()){
-                footPrint[c-'a']++;
-            }
-            String footPrintString = String.valueOf(footPrint);
-            ans.putIfAbsent(footPrintString, new ArrayList<>());
-            ans.get(footPrintString).add(s);
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String sortedShit = String.valueOf(c);
+            ans.putIfAbsent(sortedShit, new ArrayList<>());
+            ans.get(sortedShit).add(s);
         }
         return new ArrayList<>(ans.values());
     }
