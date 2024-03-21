96a3f32fa4c17a5a1874f50cf4a96f5ea3ff025f
Kevin <kevinabishekjad@gmail.com>
2024-03-21 23:33:51 +0530

    Time: 2 ms (60.45%) | Memory: 41.5 MB (37.83%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,32 +1,33 @@
 class Solution {
-    // public boolean isSubsequence(String s, String t) {
-    //     int i = 0, j = 0;
-    //     while(i<s.length() && j < t.length()){
-    //         if(s.charAt(i) == t.charAt(j)) {
-    //             i++;j++;
-    //         } else {
-    //             j++;
-    //         }
-    //     }
-    //     return i == s.length();
-    // }
-
     public boolean isSubsequence(String s, String t) {
-        List<Integer>[] map = new List[256];
-        for(int i = 0;i<t.length();i++){
-            if(map[t.charAt(i)] == null){
-                map[t.charAt(i)] = new ArrayList<>();
+        int i = 0, j = 0;
+        
+        while(i<s.length() && j < t.length()){
+            if(s.charAt(i) == t.charAt(j)) {
+                i++;j++;
+            } else {
+                j++;
             }
-            map[t.charAt(i)].add(i);
         }
-        int p = 0;
-        for (int i = 0;i<s.length();i++){
-            if(map[s.charAt(i)] == null) return false;
-            int l = Collections.binarySearch(map[s.charAt(i)],p);
-            if(l<0)l = -l -1;
-            if(l == map[s.charAt(i)].size()) return false;
-            p = map[s.charAt(i)].get(l)+1;
-        }
-        return true;
+        return i == s.length();
     }
+
+    // public boolean isSubsequence(String s, String t) {
+    //     List<Integer>[] map = new List[256];
+    //     for(int i = 0;i<t.length();i++){
+    //         if(map[t.charAt(i)] == null){
+    //             map[t.charAt(i)] = new ArrayList<>();
+    //         }
+    //         map[t.charAt(i)].add(i);
+    //     }
+    //     int p = 0;
+    //     for (int i = 0;i<s.length();i++){
+    //         if(map[s.charAt(i)] == null) return false;
+    //         int l = Collections.binarySearch(map[s.charAt(i)],p);
+    //         if(l<0)l = -l -1;
+    //         if(l == map[s.charAt(i)].size()) return false;
+    //         p = map[s.charAt(i)].get(l)+1;
+    //     }
+    //     return true;
+    // }
 }
