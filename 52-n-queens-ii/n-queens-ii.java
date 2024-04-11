9016fd9c42dce310c582bb1e91538755b6eb0b66
Kevin <kevinabishekjad@gmail.com>
2024-04-11 12:29:17 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (88.34%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.java
+++ 52-n-queens-ii/n-queens-ii.java
@@ -1,44 +1,33 @@
 class Solution {
     int count = 0;
     public int totalNQueens(int n) {
-        int[][] board = new int[n][n];
-        helper(board,0,n);
+        count=0;
+        boolean [] columns = new boolean[n];
+        boolean[] d1 = new boolean[2*n];
+        boolean[] d2 = new boolean[2*n];
+
+        helper(columns, d1, d2, 0, n);
         return count;
     }
 
-    public void helper(int[][] board, int i, int n){
-        if(i>=n){
+    public void helper(boolean[] columns, boolean[] d1, boolean[] d2, int i, int n){
+        if(i==n){
             count++;
             return;
         }
         for(int j = 0;j<n;j++){
-            if(canPlace(i,j,board,n)){
-                board[i][j] = 1;
-                helper(board,i+1,n);
+            int ii = j-i+n;
+            int jj = i+j;
+            if((columns[j] || d1[ii] || d2[jj])){
+                continue;
             }
-            board[i][j] = 0;
+            columns[j] = true;
+                d1[ii] = true;
+                d2[jj] = true;
+                helper(columns, d1, d2, i+1, n);
+                columns[j] = false;
+                d1[ii] = false;
+                d2[jj] = false;
         }
-    }
-
-    public boolean canPlace(int i, int j, int[][] board, int n){
-        int I,J;
-        for(I = 0;I<=i;I++){
-            if(board[I][j] == 1) return false;
-        }
-        I = i;
-        J = j;
-        while(I>=0 && J>=0){
-            if(board[I][J] == 1) return false;
-            I--;
-            J--;
-        }
-        I = i;
-        J = j;
-        while(I>=0 && J<n){
-            if(board[I][J] == 1) return false;
-            I--;
-            J++;
-        }
-        return true;
     }
 }
