fc2de69e8f0555fc94417e6ea7bf39601c73e3cd
Kevin <kevinabishekjad@gmail.com>
2024-05-04 22:57:27 +0530

    Time: 3 ms (37.41%) | Memory: 47.6 MB (51.02%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -6,7 +6,7 @@ class Solution {
             parent[i] = i;
         }
         for(int i = 0;i<n;i++){
-            for(int j = 0;j<n;j++){
+            for(int j = i+1;j<n;j++){
                 if(isConnected[i][j] == 1){
                     union(parent, i, j);
                 }
