d59952f2118f3e0822b9f9a1cc17287c8fd0316b
Kevin <kevinabishekjad@gmail.com>
2024-05-05 16:36:08 +0530

    Time: 1 ms (87.96%) | Memory: 48.1 MB (18.13%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,32 +1,23 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
+        int count = 0;
         int n = isConnected.length;
-        int[] parent = new int[n];
-        for(int i = 0;i<n;i++){
-            parent[i] = i;
-        }
-        for(int i = 0;i<n;i++){
-            for(int j = i+1;j<n;j++){
-                if(isConnected[i][j] == 1){
-                    union(parent, i, j);
-                }
+        boolean[] visited = new boolean[n];
+        for(int i = 0; i< n; i++){
+            if(!visited[i]){
+                dfs(isConnected, i, visited);
+                count++;
             }
         }
-        int result = 0;
-        for(int i = 0;i<n;i++){
-            if(parent[i] == i) result++;
+        return count;
+    }
+
+    public void dfs(int[][] g, int i, boolean[] visited) {
+        for(int j = 0;j<g.length;j++){
+            if(g[i][j] == 1 && !visited[j]){
+                visited[j] = true;
+                dfs(g, j, visited);
+            }
         }
-        return result;
-    }
-
-    public void union(int[] parent, int i, int j){
-        int a = find(parent, i);
-        int b = find(parent, j);
-        parent[a] = b;
-    }
-
-    public int find(int[] parent, int i){
-        if(i == parent[i]) return i;
-        else return find(parent, parent[i]);
     }
 }
