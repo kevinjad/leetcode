4d577d067bd798f6d8cce395e5e3e98cf63fa808
Kevin <kevinabishekjad@gmail.com>
2024-07-02 23:05:59 +0530

    Time: 1 ms (100.00%) | Memory: 42.4 MB (21.38%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,20 +1,27 @@
 class Solution {
     public boolean isMatch(String a, String b){
-        return reghelp(0,0,a,b);
+        Boolean[][] mem = new Boolean[a.length()+1][b.length()+1];
+        return reghelp(0,0,a,b, mem);
     }
 
-    private boolean reghelp(int i, int j, String a, String b) { // aab a*b
+    private boolean reghelp(int i, int j, String a, String b, Boolean[][] mem) { // aab a*b
         if(i >= a.length() && j >= b.length()) return true;
         if(j >= b.length())return false;
 
+        if(mem[i][j] != null) return mem[i][j]==true;
+        Boolean result;
         boolean match = i<a.length() && (a.charAt(i) == b.charAt(j) || b.charAt(j) == '.');
         if (j+1<b.length() && b.charAt(j+1) == '*'){
-            return (reghelp(i,j+2,a,b) || (match && reghelp(i+1,j,a,b)));
+            result= (reghelp(i,j+2,a,b, mem) || (match && reghelp(i+1,j,a,b, mem)));
+            mem[i][j] = result;
+            return result==true;
         }
         if(match) {
-            return reghelp(i+1,j+1, a, b);
+            result= reghelp(i+1,j+1, a, b, mem);
         } else {
-            return false;
+            result= false;
         }
+        mem[i][j] = result;
+        return result==true;
     }
 }
